217943dc4e1a43059636c8f510ff5c36e36b10dc
Abhinandan Goyal <abhinandan0323.cse19@chitkara.edu.in>
2021-10-20 11:38:25 +0530

    emoji search app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react'
+import EmojiData from './emogi.json';
 import './App.css';
+const App = () => {
+  const [search,setSearch] = useState("");
+  const [data,setData] = useState([]);
 
-function App() {
+  useEffect(()=>{
+      const newData = EmojiData.filter(emoji => emoji.title.toLowerCase().includes(search.toLowerCase()));
+      setData(newData);
+  },[search])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="nav">
+      <center>
+        <h1> Emoji Search</h1>
+        <input size="30" type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+        </center>
+      </div>
+      <div className="container">
+        {data.map(emoji =>
+         
+            <div className="card" key={emoji.title}>
+          <div className="card-body" onClick={() => {navigator.clipboard.writeText(emoji.symbol);alert("Emoji Copy")}}>
+            {emoji.symbol} {emoji.title}
+          </div>
+          </div>
+        
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
